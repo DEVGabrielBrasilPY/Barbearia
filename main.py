5925963c9c1a19523948265ff6b64323bfb7097e
DEVGabrielBrasilPY <127884139+DEVGabrielBrasilPY@users.noreply.github.com>
2023-03-23 16:12:32 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+#Barbearia 77
+print('Seja bem vindo!!')
+nome = input("qual seu nome ? ")
+servicos = 'corte,hidratação,barba,bigode'
+dia = 'segunda,terça,quarta,quinta,sexta'
+str(input('Qual serviço você deseja {} ? '.format(nome)))
+profissionais: 'Alan,Antony,Pablo'
+profissionais = str(input('Qual profissional você deseja {} ? '.format(nome)))
+dia = str(input('Qual dia você deseja ?'.format(dia)))
+horario ='9,10,11,13,14,15,16,17,18'
+horario = int(input('Qual horario você deseja ?'.format(horario)))
+print('Seu horario foi agendado {}, até as '.format(nome) +str(horario) + 'h, de ' + str(dia))
